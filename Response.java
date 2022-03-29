507b48e8ea937d93af16e1a46ebc836059b196b3
Matthew Micale <69608984+Matthew-Micale@users.noreply.github.com>
2022-03-29 15:09:34 -0400

    Create Response.java

---
+++ Response.java
@@ -0,0 +1,37 @@
+public class Response
+{
+  public PersonalityType type;
+  public Question questionAnswered;
+  public int scaledResponse;
+  
+  public Response(Question q, int sr)
+  {
+	  questionAnswered = q;
+	  scaledResponse = sr;
+	  
+  }
+  
+  public Trait getDelta()
+  {
+	if(scaledResponse > 3)
+	{
+		return questionAnswered.getTrait2();
+	}
+	else if(scaledResponse < 3)
+	{
+		return questionAnswered.getTrait1();
+	}
+	else
+	{
+		return questionAnswered.getTrait1();
+	}
+  }
+  
+  public int getWeight()
+  {
+	  int w = questionAnswered.getQWeight();
+	  //formula for calculating total score towards personality type here
+	  return w;
+  }
+  }
+  
