1207ad217a3abba21f6853548ef825928b4864da
Matthew Micale <69608984+Matthew-Micale@users.noreply.github.com>
2022-03-29 15:06:14 -0400

    Create Career.java

---
+++ Career.java
@@ -0,0 +1,22 @@
+public class Career
+{
+  public String jobTitle;
+  public FinalPersonalityType pt1;
+  public FinalPersonalityType pt2;
+  
+  public Career(String jt, FinalPersonalityType p1, FinalPersonalityType p2)
+  {
+	  jobTitle = jt;
+	  pt1 = p1;
+	  pt2 = p2;
+	  
+  }
+  
+  public boolean checkMatch(FinalPersonalityType temp)
+  {
+	  if(temp == pt1 || temp == pt2)
+		  return true;
+	  else
+		  return false;
+  }
+  }
