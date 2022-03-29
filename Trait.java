be8067b954efc78b907566f6f17e0b5c8567fa17
Matthew Micale <69608984+Matthew-Micale@users.noreply.github.com>
2022-03-29 15:10:00 -0400

    Create Trait.java

---
+++ Trait.java
@@ -0,0 +1,18 @@
+public class Trait
+{
+	public String traitName;
+	public int traitId;
+	
+	public Trait(String tn, int id)
+	{
+		
+	traitName = tn;
+	traitId = id;
+		
+	}
+	
+	public int getId()
+	{
+		return traitId;
+	}
+}
